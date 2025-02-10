e4fde9e360ca76489db1924f49ab5f4aa5b10461
Marten-Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-02-10 12:29:06 +0100

    try 3

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
@@ -15,9 +9,7 @@ const config = {
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-	},
-
-	extensions: ['.svelte', '.svx']
+	}
 };
 
 export default config;
